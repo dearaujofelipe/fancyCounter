a6e22034adf944747ef11d5f0cc82c17834063a4
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-02 20:55:32 -0300

    components + css setup

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import Count from './Count';
+import CountButtons from './CountButtons';
+import ResetButton from './ResetButton';
+import Title from './Title';
+
+function Card() {
+  return (
+    <div className="card">
+      <Title />
+      <Count />
+      <ResetButton />
+      <CountButtons />
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/ResetButton.jsx
@@ -0,0 +1,11 @@
+import { ResetIcon } from '@radix-ui/react-icons';
+
+function ResetButton() {
+  return (
+    <button className="reset-btn">
+      <ResetIcon className="reset-btn-icon" />
+    </button>
+  );
+}
+
+export default ResetButton;
